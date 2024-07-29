99f88d0fb824f8256945984fd03ead16b7ca0b0c
esteban <jepv1020@gmail.com>
2024-07-29 13:50:54 -0500

    feature/EP-14-create-controller

--- src/main/java/com/assessment/riwi/api/abstract_controllers/IProductController.java
+++ src/main/java/com/assessment/riwi/api/abstract_controllers/IProductController.java
@@ -1,8 +1,8 @@
 package com.assessment.riwi.api.abstract_controllers;
 
-import com.assessment.riwi.api.abstract_controllers.generic.CreateController;
+import com.assessment.riwi.api.abstract_controllers.generic.ReadAllController;
 import com.assessment.riwi.api.dtos.responses.ProductResponse;
 
 public interface IProductController extends
-        CreateController <ProductResponse, ProductResponse> {
+        ReadAllController <ProductResponse> {
 }
--- src/main/java/com/assessment/riwi/api/controllers/ProductControllerImpl.java
+++ src/main/java/com/assessment/riwi/api/controllers/ProductControllerImpl.java
@@ -1,18 +1,63 @@
 package com.assessment.riwi.api.controllers;
 
 import com.assessment.riwi.api.abstract_controllers.IProductController;
+import com.assessment.riwi.api.dtos.responses.OrderResponse;
 import com.assessment.riwi.api.dtos.responses.ProductResponse;
+import com.assessment.riwi.infrastructure.abstract_services.IProductService;
+
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
+
+import org.springframework.data.domain.Page;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name="product")
 @RestController
-@RequestMapping("/orders")
+@RequestMapping("/products")
 @AllArgsConstructor
 public class ProductControllerImpl  implements IProductController {
+
+    private final IProductService productService;
+
+    @ApiResponse(
+            responseCode = "200",
+            description = "Successfully created the product",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ProductResponse.class))
+    )
+    @ApiResponse(
+            responseCode = "400",
+            description = "incorrect request format in invalid request",
+            content = {
+                    @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)
+                    )
+            }
+    )
+    @ApiResponse(
+            responseCode = "404",
+            description = "id not found",
+            content = {
+                    @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)
+                    )
+            }
+    )
     @Override
-    public ResponseEntity<ProductResponse> create(ProductResponse request) {
-        return null;
+    public ResponseEntity<Page<ProductResponse>> readAll( @RequestParam(defaultValue = "0") int page,@RequestParam(defaultValue = "10") int size) {
+            
+            Page<ProductResponse> productPage = productService.readAll(page, size);
+            return ResponseEntity.ok(productPage);
+        }
     }
-}
+    
+

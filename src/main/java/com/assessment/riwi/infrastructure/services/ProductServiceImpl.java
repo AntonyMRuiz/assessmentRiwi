1d2a89e4cbe67de1c2ee2ad87ab0a463adce0205
danielsm <danielsanchezm92@gmail.com>
2024-07-29 13:33:26 -0500

    user service implemented

--- src/main/java/com/assessment/riwi/infrastructure/services/ProductServiceImpl.java
+++ src/main/java/com/assessment/riwi/infrastructure/services/ProductServiceImpl.java
@@ -3,12 +3,17 @@ package com.assessment.riwi.infrastructure.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
 
 import com.assessment.riwi.api.dtos.responses.ProductResponse;
 import com.assessment.riwi.domain.repositories.ProductRepository;
 import com.assessment.riwi.infrastructure.abstract_services.IProductService;
 import com.assessment.riwi.infrastructure.mappers.ProductMapper;
 
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
 public class ProductServiceImpl implements IProductService {
 
     @Autowired
--- src/main/java/com/assessment/riwi/infrastructure/services/UserServiceImpl.java
+++ src/main/java/com/assessment/riwi/infrastructure/services/UserServiceImpl.java
@@ -1,6 +1,32 @@
 package com.assessment.riwi.infrastructure.services;
 
-import com.assessment.riwi.infrastructure.abstract_services.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.assessment.riwi.api.dtos.requests.UserRequest;
+import com.assessment.riwi.api.dtos.responses.UserResponse;
+import com.assessment.riwi.domain.entities.UserEntity;
+import com.assessment.riwi.domain.repositories.UserRepository;
+import com.assessment.riwi.infrastructure.abstract_services.IUserService;
+import com.assessment.riwi.infrastructure.mappers.UserMapper;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
 public class UserServiceImpl implements IUserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Override
+    public UserResponse create(UserRequest request) {
+
+        UserEntity user = this.userMapper.toEntity(request);
+        return this.userMapper.toResponse(this.userRepository.save(user));
+
+    }
 }

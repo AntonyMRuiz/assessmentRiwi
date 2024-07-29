78ae7ceaa2d4eadb69c7d97a2ff84dab9dc6040c
antony martinez <antonymaruiz02@gmail.com>
2024-07-29 13:17:58 -0500

    Fix: fixed bug

--- src/main/java/com/assessment/riwi/infrastructure/services/DiscountServiceImpl.java
+++ src/main/java/com/assessment/riwi/infrastructure/services/DiscountServiceImpl.java
@@ -1,6 +1,38 @@
 package com.assessment.riwi.infrastructure.services;
 
+import com.assessment.riwi.api.dtos.requests.DiscountRequest;
+import com.assessment.riwi.api.dtos.responses.DiscountResponse;
+import com.assessment.riwi.domain.repositories.DiscountRepository;
 import com.assessment.riwi.infrastructure.abstract_services.IDiscountService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
 
+@Service
+@AllArgsConstructor
 public class DiscountServiceImpl implements IDiscountService {
+
+    @Autowired
+    private final DiscountRepository discountRepository;
+
+    @Override
+    public DiscountResponse create(DiscountRequest request) {
+        return discountRepository.save();
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+
+    }
+
+    @Override
+    public Page<DiscountResponse> readAll(int page, int size) {
+        return null;
+    }
+
+    @Override
+    public DiscountResponse updateById(Long aLong, DiscountRequest request) {
+        return null;
+    }
 }
